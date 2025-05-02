52685070d52f69b4a7faf58ce9e31765f48f7707
Wiam <wiam.feddouli@etu.uae.ac.ma>
2025-05-02 21:07:24 +0100

    Updated details css and added a new eventEmitter to close the details Modal popup

--- src/product-details-component/product-details.component.ts
+++ src/product-details-component/product-details.component.ts
@@ -1,16 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Product } from '../models/Product';
-import { Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product-details-component',
+  standalone: true,
   imports: [FormsModule, CommonModule],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.css'
 })
 export class ProductDetailsComponent {
   @Input() product: Product | null = null;
+  @Output() close = new EventEmitter<void>();
 
+  closeModal() {
+    this.close.emit();
+  }
 }
